1b3f38056442a76c41837ee4a7532fbfd9d01048
Pianoの黑白键 <2372412971.com>
2017-03-04 15:48:02 +0800

    json

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
+
 class Blog extends React.Component {
-  componentsDidMount(){
-    axios.get('https://raw.githubusercontent.com/newming/demodata/master/duopingshidai.json').then(res=>console.log(res))
+  componentDidMount(){
+    axios.get('https://raw.githubusercontent.com/webzcl/myblog/master/Blogs/blogs.json')
+    .then(res=>console.log(res))
   }
   render () {
     return(
-  <div>
-    blog
-  </div>
+      <div>
+        blog
+      </div>
     )
   }
 }
