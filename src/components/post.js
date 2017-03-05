83c399f84567cdd09371ba733500587b1173db08
Pianoの黑白键 <2372412971.com>
2017-03-05 09:48:59 +0800

    3

--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import axios from 'axios';
+import marked from 'marked'
 class Post extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:''
+    }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/happylrx/myblog/master/Blogs/${this.props.params.title}.md`)
+    .then(res=>this.setState({data:res.data}))
+  }
   render () {
     return(
       <div>
-        {this.props.params.title}
+        {this.state.data.length==0?'正在加载中':
+        <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>
+      }
       </div>
     )
   }
