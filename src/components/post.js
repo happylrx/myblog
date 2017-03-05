42f08d1521498e0938bebbaf2a70ae80b1c3e583
Pianoの黑白键 <2372412971.com>
2017-03-05 09:29:49 +0800

    内容2

---
+++ src/components/post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Post extends React.Component {
+  render () {
+    return(
+      <div>
+        {this.props.params.title}
+      </div>
+    )
+  }
+}
+export default Post;
--- src/routers.js
+++ src/routers.js
@@ -4,6 +4,7 @@ import Home from './components/home.js'
 import Blog from './components/blog.js'
 import Work from './components/work.js'
 import About from './components/about.js'
+import Post from './components/post.js'
 import {Router,Route,browserHistory,IndexRoute} from 'react-router'
 
 export default function() {
@@ -14,6 +15,7 @@ export default function() {
             <Route path='blog' component={Blog}/>
             <Route path='about' component={About}/>
             <Route path='work' component={Work}/>
+            <Route path='post/:title' component={Post}/>
           </Route>
         </Router>
       )
