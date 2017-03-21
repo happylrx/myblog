5645b5f97350366463e12df2160f6506f194c9e7
Pianoの黑白键 <2372412971.com>
2017-03-21 11:18:31 +0800

    cshi

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
       <div className='home'>
         <div className='home-cover'>
           <div>
-          <h1 style={{color:'white'}}>I'M LiuRuiXin</h1>
+          <h1 style={{color:'white'}}>White Album</h1>
           <p style={{color:'white'}}>FrontEnd Web Developer</p>
           <a href='https://github.com/happylrx'>MY GITHUB</a>
           </div>
